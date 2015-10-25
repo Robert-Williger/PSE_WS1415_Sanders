d9945239aef25cbcc70d0fd3ca5c40f5df27e4cb
Alex-Gellner <uqdpf@student.kit.edu>
2015-10-25 22:59:28 +0100

    Updated TextProcessor.

--- src/model/AdvancedTextProcessor.java
+++ src/model/AdvancedTextProcessor.java
@@ -13,23 +13,35 @@ import model.map.IMapManager;
 public class AdvancedTextProcessor implements ITextProcessor {
     private final int maxDistance;
     private final int suggestions;
+    private final BoundedHeap<Tuple> heap;
     private final int[][] distance;
-    private TreeNode indexRoot;
+    private final SortedTree indexRoot;
     private int maxStringLength;
 
-    public AdvancedTextProcessor(final Entry[][] entries, final Label[] labels, final IMapManager manager,
-            final int suggestions) {
-
-        this.maxDistance = 2;
-        this.suggestions = suggestions;
-
-        setupIndex(entries, labels, manager);
-        distance = createDistanceArray();
-        maxStringLength += 2 * maxDistance;
+    public AdvancedTextProcessor() {
+        this(new Entry[0][], new Label[0], null);
     }
 
-    private void setupIndex(final Entry[][] entries, final Label[] labels, final IMapManager manager) {
-        indexRoot = new TreeNode("", null, null);
+    public AdvancedTextProcessor(final Entry[][] entries, final Label[] labels, final IMapManager manager) {
+        this(entries, labels, manager, 5, 2);
+    }
+
+    public AdvancedTextProcessor(final Entry[][] entries, final Label[] labels, final IMapManager manager,
+            final int suggestions, final int maxDistance) {
+
+        this.suggestions = suggestions;
+        this.heap = new BoundedHeap<Tuple>(suggestions);
+        this.maxDistance = maxDistance;
+
+        final Tree unsortedRoot = setupIndex(entries, labels, manager);
+        distance = createDistanceArray();
+        maxStringLength += 2 * maxDistance;
+
+        indexRoot = unsortedRoot.toSortedTree();
+    }
+
+    private Tree setupIndex(final Entry[][] entries, final Label[] labels, final IMapManager manager) {
+        final Tree root = new Tree("", null, null);
 
         maxStringLength = 0;
         for (final Entry[] entry : entries) {
@@ -39,8 +51,8 @@ public class AdvancedTextProcessor implements ITextProcessor {
                 final String realName = streetName + " " + cityName;
                 final StreetNode node = new StreetNode(0.5f, entry[i].getStreet());
                 // TODO order by size of city
-                add(normalize(realName), realName, node);
-                add(normalize(cityName + " " + streetName), realName, node);
+                add(normalize(realName), realName, root, node);
+                add(normalize(cityName + " " + streetName), realName, root, node);
             }
         }
 
@@ -48,8 +60,21 @@ public class AdvancedTextProcessor implements ITextProcessor {
         for (final Label label : labels) {
             final AddressNode addressNode = manager.getAddressNode(label.getLocation());
             if (addressNode != null) {
-                add(normalize(label.getName()), label.getName(), addressNode.getStreetNode());
+                add(normalize(label.getName()), label.getName(), root, addressNode.getStreetNode());
             }
+        }
+
+        return root;
+    }
+
+    private void printIndex(final SortedTree root, final int depth) {
+        String spaces = "";
+        for (int i = 0; i < depth; i++) {
+            spaces += "  ";
+        }
+        System.out.println(spaces + "[" + depth + "] " + root.getIndexName());
+        for (final SortedTree child : root.getChildren()) {
+            printIndex(child, depth + 1);
         }
     }
 
@@ -66,20 +91,20 @@ public class AdvancedTextProcessor implements ITextProcessor {
         return distance;
     }
 
-    private void add(final String normalizedName, final String realName, final StreetNode streetNode) {
+    private void add(final String normalizedName, final String realName, final Tree tree, final StreetNode streetNode) {
         if (normalizedName.length() > maxStringLength) {
             maxStringLength = normalizedName.length();
         }
-        add(normalizedName, realName, streetNode, indexRoot, 0);
+        add(normalizedName, realName, streetNode, tree, 0);
     }
 
-    private void add(final String normalizedName, final String realName, final StreetNode streetNode,
-            final TreeNode root, int index) {
+    private void add(final String normalizedName, final String realName, final StreetNode streetNode, final Tree root,
+            int index) {
 
         if (normalizedName.length() > 1) {
-            for (final Iterator<TreeNode> iterator = root.getChildren().iterator(); iterator.hasNext();) {
-                final TreeNode child = iterator.next();
-                final String childName = child.getName();
+            for (final Iterator<Tree> iterator = root.getChildren().iterator(); iterator.hasNext();) {
+                final Tree child = iterator.next();
+                final String childName = child.getIndexName();
 
                 if (normalizedName.charAt(index) == childName.charAt(index)) {
                     ++index;
@@ -87,22 +112,22 @@ public class AdvancedTextProcessor implements ITextProcessor {
                     while (index < length) {
                         if (normalizedName.charAt(index) != childName.charAt(index)) {
                             iterator.remove();
-                            final TreeNode splitNode = new TreeNode(normalizedName.substring(0, index), null, null);
+                            final Tree splitNode = new Tree(normalizedName.substring(0, index), null, null);
                             splitNode.addChild(child);
-                            splitNode.addChild(new TreeNode(normalizedName, realName, streetNode));
+                            splitNode.addChild(new Tree(normalizedName, realName, streetNode));
                             root.getChildren().add(splitNode);
                             return;
                         }
                         ++index;
                     }
                     if (normalizedName.length() == index) {
-                        if (child.getName().length() != index) {
+                        if (child.getIndexName().length() != index) {
                             iterator.remove();
-                            final TreeNode splitNode = new TreeNode(normalizedName, realName, streetNode);
+                            final Tree splitNode = new Tree(normalizedName, realName, streetNode);
                             splitNode.addChild(child);
                             root.getChildren().add(splitNode);
-                        } else if (child.getRealName() == null) {
-                            child.setRealName(realName);
+                        } else if (child.getName() == null) {
+                            child.setName(realName);
                             child.setStreetNode(streetNode);
                         }
                         return;
@@ -113,72 +138,137 @@ public class AdvancedTextProcessor implements ITextProcessor {
 
             }
 
-            root.getChildren().add(new TreeNode(normalizedName, realName, streetNode));
+            root.getChildren().add(new Tree(normalizedName, realName, streetNode));
         }
     }
 
     @Override
     public List<String> suggest(final String address) {
-        final BoundedHeap<Tuple> tuples = new BoundedHeap<Tuple>(suggestions);
+        // long start = System.currentTimeMillis();
+
         final String normalizedAddress = normalize(address);
 
-        suggest(normalizedAddress, indexRoot, tuples);
+        suggestBinary(normalizedAddress, indexRoot, 0);
+        // System.out.println(count);
 
-        int length = Math.min(suggestions, tuples.size());
+        int length = Math.min(suggestions, heap.size());
 
         final String[] choice = new String[length];
         for (int i = 0; i < length; i++) {
-            choice[length - i - 1] = tuples.deleteMax().getName();
+            choice[length - i - 1] = heap.deleteMax().getName();
         }
 
+        // System.out.println(System.currentTimeMillis() - start);
         return Arrays.asList(choice);
     }
 
-    private void suggest(final String input, final TreeNode node, final BoundedHeap<Tuple> heap) {
-        int distance = prefixEditDistance(input, node.getName());
+    private void suggestBinary(final String input, final SortedTree tree, int index) {
+        int distance = prefixEditDistance(input, tree.getIndexName());
 
         if (distance <= maxDistance) {
             if (heap.size() == suggestions) {
                 Tuple max = heap.max();
-                if (distance > max.getDistance() || distance == max.getDistance()
-                        && node.getName().length() > max.getName().length()) {
+                if (max.getDistance() == 0 || distance >= max.getDistance()) {
                     return;
                 }
             }
 
-            if (node.getRealName() != null && node.getName().length() >= input.length() - 1) {
-                heap.insert(new Tuple(node.getRealName(), distance));
+            if (tree.getName() != null && tree.getIndexName().length() >= input.length() - 1) {
+                heap.insert(new Tuple(tree.getName(), distance));
             }
 
-            for (final TreeNode child : node.getChildren()) {
-                suggest(input, child, heap);
+            final SortedTree child = binarySearch(tree, input, index);
+            if (child != null) {
+                if (input.length() > (child.getIndexName().length())) {
+                    suggestBinary(input, child, child.getIndexName().length());
+                } else {
+                    suggest(input, child);
+                }
+            }
+
+            for (final SortedTree node : tree.getChildren()) {
+                if (node != child) {
+                    suggest(input, node);
+                }
+            }
+        }
+    }
+
+    private void suggest(final String input, final SortedTree tree) {
+        int distance = prefixEditDistance(input, tree.getIndexName());
+
+        if (tree.getIndexName().equals("eppingerstrassekraichtal")) {
+            System.out.println(distance);
+        }
+        if (distance <= maxDistance) {
+            if (heap.size() == suggestions) {
+                Tuple max = heap.max();
+                if (max.getDistance() == 0 || distance >= max.getDistance()) {
+                    return;
+                }
+            }
+
+            if (tree.getName() != null && tree.getIndexName().length() >= input.length() - 1) {
+                heap.insert(new Tuple(tree.getName(), distance));
+            }
+
+            for (final SortedTree node : tree.getChildren()) {
+                if (tree.getIndexName().equals("eppinger")) {
+                    System.out.println(node.getIndexName());
+                }
+                suggest(input, node);
             }
         }
     }
 
     @Override
     public StreetNode parse(final String address) {
-        return parse(normalize(address), indexRoot, 0);
+        return parse(indexRoot, normalize(address), 0);
     }
 
-    private StreetNode parse(final String address, final TreeNode root, int index) {
-        for (final TreeNode child : root.getChildren()) {
-            final String childName = child.getName();
-            if (childName.charAt(index) == address.charAt(index)) {
-                ++index;
-                while (index < childName.length()) {
-                    if (childName.charAt(index) != address.charAt(index)) {
-                        return null;
-                    }
-                    ++index;
-                }
-                return index != address.length() ? parse(address, child, index) : child.getStreetNode();
+    private StreetNode parse(final SortedTree root, final String input, int index) {
+        final SortedTree child = binarySearch(root, input, index);
+        int childLength;
+        if (child == null || (childLength = child.getIndexName().length()) > input.length()) {
+            return null;
+        }
+
+        while (index < childLength) {
+            if (child.getIndexName().charAt(index) != input.charAt(index)) {
+                return null;
+            }
+            ++index;
+        }
+
+        return index == input.length() ? child.getStreetNode() : parse(child, input, index);
+    }
+
+    // binary search looks for child with same character as input at given index
+    private SortedTree binarySearch(final SortedTree root, final String input, int index) {
+        final SortedTree[] children = root.getChildren();
+
+        int left = 0;
+        int right = children.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            int dif = children[middle].getIndexName().charAt(index) - input.charAt(index);
+
+            if (dif == 0) { // maybe found
+                return children[middle];
+            }
+
+            if (dif < 0) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
             }
         }
 
         return null;
     }
 
+    // TODO suggestions for 'Eppsilnger' but not for 'Eppsilnge' atm!
     private int prefixEditDistance(String a, String b) {
         if (a.length() > b.length()) {
             String temp = b;
@@ -186,7 +276,7 @@ public class AdvancedTextProcessor implements ITextProcessor {
             a = temp;
         }
 
-        int minDist = maxDistance + 1;
+        int minDist = Integer.MAX_VALUE;
         // TODO necessary?
         int bLength = Math.min(b.length(), maxStringLength);
 
@@ -223,6 +313,7 @@ public class AdvancedTextProcessor implements ITextProcessor {
             }
         }
 
+        // TODO return Math.min(minDist, currentMinDist); ?
         return minDist;
 
     }
@@ -240,7 +331,9 @@ public class AdvancedTextProcessor implements ITextProcessor {
         replaceAllSB(nameSB, "ä", "ae");
         replaceAllSB(nameSB, "ü", "ue");
         replaceAllSB(nameSB, "ö", "oe");
-        replaceAllSB(nameSB, "-", " ");
+        // TODO is that reasonable?
+        replaceAllSB(nameSB, " ", "");
+        replaceAllSB(nameSB, "-", "");
 
         return nameSB.toString();
     }
@@ -283,45 +376,100 @@ public class AdvancedTextProcessor implements ITextProcessor {
 
     }
 
-    private static class TreeNode {
-        private final List<TreeNode> children;
-        private final String name;
-        private StreetNode streetNode;
-        private String realName;
+    private static class Tree {
+        private static final SortedTree[] emptyChildren = new SortedTree[0];
 
-        public TreeNode(final String name, final String realName, final StreetNode streetNode) {
+        private String name;
+        private final String indexName;
+        private final List<Tree> children;
+        private StreetNode streetNode;
+
+        public Tree(final String indexName, final String name, final StreetNode streetNode) {
+            this.indexName = indexName;
             this.name = name;
-            this.realName = realName;
             this.streetNode = streetNode;
-            children = new LinkedList<TreeNode>();
+            children = new LinkedList<Tree>();
         }
 
-        public void addChild(final TreeNode node) {
+        public void addChild(final Tree node) {
             children.add(node);
         }
 
-        public String getName() {
-            return name;
+        public String getIndexName() {
+            return indexName;
         }
 
         public void setStreetNode(final StreetNode node) {
             this.streetNode = node;
         }
 
+        public List<Tree> getChildren() {
+            return children;
+        }
+
+        public void setName(final String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public SortedTree toSortedTree() {
+            final SortedTree[] children;
+
+            if (!this.children.isEmpty()) {
+
+                children = new SortedTree[this.children.size()];
+
+                int count = -1;
+                for (final Tree node : this.children) {
+                    children[++count] = node.toSortedTree();
+                }
+
+                Arrays.sort(children);
+
+            } else {
+                children = emptyChildren;
+            }
+
+            return new SortedTree(indexName, name, streetNode, children);
+        }
+    }
+
+    private static class SortedTree implements Comparable<SortedTree> {
+        private final String name;
+        private final String indexName;
+        private final SortedTree[] children;
+        private final StreetNode streetNode;
+
+        public SortedTree(final String indexName, final String name, final StreetNode streetNode,
+                final SortedTree[] children) {
+            this.indexName = indexName;
+            this.name = name;
+            this.streetNode = streetNode;
+            this.children = children;
+        }
+
+        public String getIndexName() {
+            return indexName;
+        }
+
         public StreetNode getStreetNode() {
             return streetNode;
         }
 
-        public List<TreeNode> getChildren() {
+        public SortedTree[] getChildren() {
             return children;
         }
 
-        public void setRealName(final String realName) {
-            this.realName = realName;
+        public String getName() {
+            return name;
         }
 
-        public String getRealName() {
-            return realName;
+        @Override
+        public int compareTo(final SortedTree o) {
+            return indexName.compareTo(o.indexName);
         }
     }
 }
